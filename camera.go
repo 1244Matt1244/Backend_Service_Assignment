8c87a81385eb7fc85e87a4180f23d41b3c175cc1
44Matt44 <matt123.3a@gmail.com>
2024-09-12 19:47:01 +0200

    Delete camera.go

--- camera.go
+++
@@ -1,8 +0,0 @@
-package camera
-
-// Camera represents a traffic camera
-type Camera struct {
-	ID        string  `json:"id"`
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-}
